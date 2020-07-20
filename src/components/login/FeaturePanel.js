632faa40e9106890d7743b003dc4186c23c7f419
Rakesh <rakeshgyawali@gmail.com>
2020-07-20 16:25:52 +0545

    State Management and Login is Done

---
+++ src/components/login/FeaturePanel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function FeaturePanel() {
+	return (
+	  <div>
+		<h1>B-Share</h1>
+		<p>Peer to Peer Book Sharing System</p>
+	  </div>
+	)
+  };
+	
+	
+
