b14eab689b6c3f9a21b4aae9a2b39d9f0107c14f
Rakesh <rakeshgyawali@gmail.com>
2020-08-17 16:16:27 +0545

    Separated Category Component

---
+++ src/components/category/CategoryForm.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function CategoryForm(props) {
+	return (
+		<form id='districtForm'>
+				<label htmlFor='name'>Category Name</label>
+				<input type='text' id='name' name='name'
+				value={props.name}
+				onChange={props.handleChange}/>
+					
+				{
+					props.isEdit ? (
+						<div className='flex-center'> 
+							<button className='btnJoin' onClick={props.handlePost}>Update</button>
+							<button id='btnCancel' className='btnJoin' onClick={props.clearText}>Cancel</button>
+						</div>
+					) : (	
+						<div className='flex-center'>
+							<button className='btnJoin' onClick={props.handlePost}>Add</button>
+							<button id='btnCancel' className='btnJoin' onClick={props.clearText}>Clear</button>
+						</div>
+					) 
+				}			
+		</form>
+	)
+}
